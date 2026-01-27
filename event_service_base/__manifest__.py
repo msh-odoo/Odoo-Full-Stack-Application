797539f3725436d4bb7eba550c8b4962516e243e
chdh-odoo <chdh@odoo.com>
2026-01-27 11:27:28 +0530

    [ADD] event_service_base: base module for event service core models
    todo: needs to clean the commit msg
    
    added installable module as well to test the base module only
    later will create sepearte commit ;)
    
    security
    Grant full CRUD access to event services and registrations for users
    Allow create/write on categories and tags while restricting delete

---
+++ event_service_base/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Event Service Base',
+    'summary': 'Foundational business logic for service event management',
+    'description': """
+Provides the foundational models and business logic for
+service events and bookings, intended to be extended
+by other modules.
+""",
+    'version': '1.0',
+    'author': 'Dhruv',
+    'license': 'LGPL-3',
+}
